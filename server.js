bb29d26499aaffe29efe3759b938322d726e1601
mdsumaiya <30854593+mdsumaiya@users.noreply.github.com>
2017-08-16 09:34:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,13 @@ app.get('/article-one', function (req, res) {
   res.send('Article one will be requested');
 });
 
+app.get('/article-two', function (req, res) {
+  res.send('Article two will be requested');
+});
+app.get('/article-three', function (req, res) {
+  res.send('Article three will be requested');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
